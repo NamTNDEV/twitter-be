92b5989fc13abc5dbe9f8343672c6b93e9a67dc6
NamTNDEV <namtndev312002@gmail.com>
2025-02-25 08:48:30 +0700

    Fix Handle Error Middleware

--- src/middlewares/errors.middlewares.ts
+++ src/middlewares/errors.middlewares.ts
@@ -4,17 +4,31 @@ import { HTTP_STATUS } from '~/constants/httpStatus';
 import { ErrorWithStatus } from '~/models/Errors';
 
 export const defaultErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof ErrorWithStatus) {
-    res.status(error.status).json(omit(error, ['status']));
+  try {
+    if (error instanceof ErrorWithStatus) {
+      res.status(error.status).json(omit(error, ['status']));
+    }
+
+    const finalErrors: any = {};
+    Object.getOwnPropertyNames(error).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(error, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(error, key)?.writable
+      ) {
+        return;
+      }
+      finalErrors[key] = error[key]
+    });
+
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalErrors.message,
+      errorInfo: omit(finalErrors, ['stack'])
+    })
+
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-
-  Object.getOwnPropertyNames(error).forEach((key) => {
-    Object.defineProperty(error, key, { enumerable: true });
-  });
-
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: error.message,
-    errorInfo: omit(error, ['stack']),
-  });
-
 };
